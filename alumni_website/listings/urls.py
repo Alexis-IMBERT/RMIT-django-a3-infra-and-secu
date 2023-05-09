9aa206b07b2375c6c74be3da3bf8c7151c15cebf
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-05-09 11:27:16 +1000

    comment url login and register that are not usefull for the moment

--- alumni_website/listings/urls.py
+++ alumni_website/listings/urls.py
@@ -5,7 +5,7 @@ urlpatterns =[
 	path('',views.home, name='home'),
     path('home/', views.home),
      path('hello/', views.hello),
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    # path('login/', views.login, name='login'),
+    # path('register/', views.register, name='register'),
     path('diploma_number/', views.diploma_number, name='diploma_number'),
 ]
