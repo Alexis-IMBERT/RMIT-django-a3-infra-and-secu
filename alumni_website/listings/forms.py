7a762d5afb310496d31438dc414fd6a7e37114d1
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-05-09 11:26:30 +1000

    adding fields in the form part

--- alumni_website/listings/forms.py
+++ alumni_website/listings/forms.py
@@ -7,6 +7,9 @@ class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(label='First Name',strip=True)
     last_name = forms.CharField(label='Last Name')
     email = forms.EmailField(label='Email address')
+    student_id = forms.CharField(label="Student ID",min_length=8,max_length=8,strip=True)
+    programe_name = forms.CharField(label="Program Name")
+    year_graduation = forms.DateField(label="Year of graduation")
 
     class Meta(UserCreationForm.Meta):
         model = User
