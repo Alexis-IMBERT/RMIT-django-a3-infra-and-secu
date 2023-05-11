7056e419671c699f7de43eaf01948c3a6b132aa5
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-05-11 20:21:12 +1000

    simplification de la BD

--- alumni_website/listings/forms.py
+++ alumni_website/listings/forms.py
@@ -3,19 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-# class UserRegistrationForm(UserCreationForm):
-#     first_name = forms.CharField(label='First Name',strip=True)
-#     last_name = forms.CharField(label='Last Name')
-#     email = forms.EmailField(label='Email address')
-#     student_id = forms.CharField(label="Student ID",min_length=8,max_length=8,strip=True)
-#     programe_name = forms.CharField(label="Program Name")
-#     year_graduation = forms.DateField(label="Year of graduation")
-
-#     class Meta(UserCreationForm.Meta):
-#         model = User
-#         fields = ('first_name', 'last_name', 'email')
-
-
 class UserRegisterForm(UserCreationForm):
     first_name = forms.CharField(label='First Name', strip=True, required=True)
     last_name = forms.CharField(label='Last Name', strip=True, required=True)
--- alumni_website/listings/models.py
+++ alumni_website/listings/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-class Alumni(models.Model):
-    """ Alumni's class definition """
-    username = models.fields.CharField(max_length=100)  # Username of the alumni
-    diploma_number = models.fields.BigIntegerField()  # Number of the diploma 
-                                                    # password will be hash in the future
-    password = models.fields.CharField(max_length=100)
+# class Alumni(models.Model):
+#     """ Alumni's class definition """
+#     username = models.fields.CharField(max_length=100)  # Username of the alumni
+#     diploma_number = models.fields.BigIntegerField()  # Number of the diploma 
+#                                                     # password will be hash in the future
+#     password = models.fields.CharField(max_length=100)
 
-    def __str__(self):
-        return f'{self.username}'
+#     def __str__(self):
+#         return f'{self.username}'
